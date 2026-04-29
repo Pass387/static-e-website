c1541ca3f396f2a02413fe9bfe4434e092ffbf6e
pass387 <ludwigweru@gmail.com>
2026-04-29 01:55:59 -0800

    updated map embed

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -62,7 +62,9 @@ export default function AboutContact() {
       <section>
         <iframe
           title="Location"
-          src="https://www.google.com/maps?q=Thika&output=embed"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.164794074515!2d37.07560727445474!3d-1.0370471989529715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f4e88b726b5fb%3A0xfaaa9b3e84deb8a8!2sKapanga%20Land%20Surveys!5e0!3m2!1sen!2ske!4v1777456007680!5m2!1sen!2ske"  
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade"
           width="90%"
           height="300"
         />
